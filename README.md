2e270b7c9d620491f6bc0dcb182ad35af1c4275e
LambdaGaming <starwars18m@gmail.com>
2022-11-23 15:21:09 -0500

    Initial addon

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# devtools-sbox
- Helpful s&box tools for addon/game developers.
+(WIP) This is a collection of tools meant to be used in developement of addons for the Sandbox game or other games that derive from Sandbox. Based off my [developement tool collection for Gmod.](https://github.com/LambdaGaming/devtools) The initial plan is to make everything that is currently included in the Gmod version, then add whatever else I feel is necessary afterwards. Suggestions are also welcome!
+
+# Tool List
+Get vector and eye angles of local player.
---
+++ code/tools/GetPosAngSelf.cs
@@ -0,0 +1,19 @@
+﻿namespace Sandbox.Tools
+{
+	[Library( "tool_getposangself", Title = "Get Self Pos and Angle", Description = "Prints position and eye angles of yourself." )]
+	public partial class GetPosAngSelfTool : BaseTool
+	{
+		public override void Simulate()
+		{
+			if ( !Host.IsServer ) return;
+
+			using ( Prediction.Off() )
+			{
+				if ( Input.Pressed( InputButton.PrimaryAttack ) )
+					Log.Info( Owner.Position.ToString() );
+				else if ( Input.Pressed( InputButton.SecondaryAttack ) )
+					Log.Info( Owner.EyeRotation.Angles().ToString() );
+			}
+		}
+	}
+}

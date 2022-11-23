ead81799645cc6201bdd89cdd7fd5802fc7fb61b
LambdaGaming <starwars18m@gmail.com>
2022-11-23 17:58:57 -0500

    Add get color tool

---
+++ code/tools/GetColor.cs
@@ -0,0 +1,23 @@
+﻿namespace Sandbox.Tools
+{
+	[Library( "tool_getcolor", Title = "Get Color", Description = "Prints color of target entity." )]
+	public partial class GetColorTool : BaseTool
+	{
+		public override void Simulate()
+		{
+			if ( !Host.IsServer ) return;
+
+			using ( Prediction.Off() )
+			{
+				if ( Input.Pressed( InputButton.PrimaryAttack ) )
+				{
+					TraceResult tr = DoTrace();
+					if ( tr.Hit && tr.Entity.IsValid() && tr.Entity is ModelEntity ent )
+					{
+						Log.Info( ent.RenderColor.Rgba );
+					}
+				}
+			}
+		}
+	}
+}

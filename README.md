dca67376f3fb654f8480fadc09038dc893aea6dd
LambdaGaming <starwars18m@gmail.com>
2022-11-25 17:36:46 -0500

    Add find entity tool

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-(WIP) This is an addon for s&box that contains a collection of tools meant to be used in developement of addons for the Sandbox game, or other games that derive from Sandbox. Based off my [developement tool collection for Gmod.](https://github.com/LambdaGaming/devtools) The initial plan is to make everything that is currently included in the Gmod version, then add whatever else I feel is necessary afterwards. Suggestions are also welcome!
+This is an addon for s&box that contains a collection of tools meant to be used in developement of addons for the Sandbox game, or other games that derive from Sandbox. Based off my [developement tool collection for Gmod.](https://github.com/LambdaGaming/devtools) Right now it includes all of the tools from the Gmod version, but suggestions for more are welcome!
 
 # Tool List
 - Get color of trace entity.
@@ -10,6 +10,8 @@
 - Get class and name of trace entity.
 - Get velocity and angular velocity of trace entity.
 - Create a boundary and get the upper and lower values of it.
-
-# Planned Tools
 - Find all entities of a specified class on the map.
+
+# Server Commands
+- devtools_findent [class name] - Used for the Find Entity tool to enter the name of the entity you want to search for. This is only temporary until I can get a UI made for this tool.
+
---
+++ code/tools/FindEntity.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Sandbox.Tools
+{
+	[Library( "tool_findentity", Title = "Find Entity", Description = "Prints velocity and angular velocity of target entity." )]
+	public partial class FindEntityTool : BaseTool
+	{
+		int Index = 0;
+		static List<Entity> EntList = new();
+
+		public override void Simulate()
+		{
+			if ( !Host.IsServer ) return;
+
+			using ( Prediction.Off() )
+			{
+				if ( Input.Pressed( InputButton.PrimaryAttack ) )
+					TeleTo( true );
+				else if ( Input.Pressed( InputButton.SecondaryAttack ) )
+					TeleTo( false );
+			}
+		}
+
+		void TeleTo( bool forward )
+		{
+			if ( EntList.Count <= 0 )
+			{
+				Log.Warning( "Use the devtools_findent command to enter an entity name." );
+				return;
+			}
+
+			if ( forward )
+				Index = ( int ) MathX.Clamp( Index + 1, 0, EntList.Count - 1 );
+			else
+				Index = ( int ) MathX.Clamp( Index - 1, 0, EntList.Count - 1 );
+
+			if ( EntList[Index].IsValid() )
+			{
+				Owner.Position = EntList[Index].Position;
+				Log.Info( $"Current Entity: {Index + 1} out of {EntList.Count}" );
+			}
+			else
+			{
+				EntList.Remove( EntList[Index] );
+				Log.Warning( $"The selected entity doesn't exist anymore. Removing from list." );
+			}
+		}
+
+		// This will be set through a UI eventually
+		[ConCmd.Server( "devtools_findent" )]
+		static void FindEntity( string parameter )
+		{
+			EntList.Clear();
+			EntList.AddRange( Entity.All.Where( x => ( x.ClassName == parameter) ) );
+			if ( EntList.Count > 0 )
+				Log.Info( $"Found {EntList.Count} entities with the specified class name on the map." );
+			else
+				Log.Info( $"No entities with the class name '{parameter}' could be found on the map." );
+		}
+	}
+}
